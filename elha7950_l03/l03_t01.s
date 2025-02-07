d94e822c798b13988a035033c57c2d07861dab37
Mohammad El-Hassan <mohammadelhsn@gmail.com>
2025-02-07 18:58:46 -0500

    l03

---
+++ elha7950_l03/l03_t01.s
@@ -0,0 +1,30 @@
+/*
+-------------------------------------------------------
+count1.s
+-------------------------------------------------------
+Author: Mohammad El-Hassan
+ID: 169067950
+Email: elha7950@mylaurier.ca
+Date: 02/07/2025
+-------------------------------------------------------
+A simple count down program (bgt)
+-------------------------------------------------------
+*/
+.org 0x1000  // Start at memory location 1000
+.text        // Code section
+.global _start
+_start:
+
+.text // code section
+// Store data in registers
+mov r3, #5  // Initialize a countdown value
+
+TOP:
+sub r3, r3, #1 // Decrement the countdown value
+cmp r3, #0  // Compare the countdown value to 0
+bge TOP         // Branch to TOP if > 0
+
+_stop:
+b _stop
+
+.end
---
+++ elha7950_l03/l03_t02.s
@@ -0,0 +1,33 @@
+/*
+-------------------------------------------------------
+count2.s
+-------------------------------------------------------
+Author: Mohammad El-Hassan
+ID: 169067950
+Email: elha7950@mylaurier.ca
+Date: 02/07/2025
+-------------------------------------------------------
+A simple count down program (bge)
+-------------------------------------------------------
+*/
+.org 0x1000  // Start at memory location 1000
+.text        // Code section
+.global _start
+_start:
+
+// Store data in registers
+LDR r3, =COUNTER  // Initialize a countdown value
+
+TOP:
+sub r3, r3, #1 // Decrement the countdown value
+cmp r3, #0  // Compare the countdown value to 0
+bge TOP   // Branch to top under certain conditions
+
+_stop:
+b _stop
+
+.data
+COUNTER: //address is 00001018 in hex
+.word 5
+
+.end
