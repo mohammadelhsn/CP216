fc2a2247912fa875e607f89105cc5631a64eea35
Mohammad El-Hassan <mohammadelhsn@gmail.com>
2025-02-14 21:25:26 -0500

    l04

---
+++ elha7950_l04/l04_t02.s
@@ -0,0 +1,42 @@
+/*
+-------------------------------------------------------
+l04_t02.s
+-------------------------------------------------------
+Author:  Mohammad El-Hassan
+ID:      169067950
+Email:   elha7950@mylaurier.ca
+Date:    2024-02-14
+-------------------------------------------------------
+A simple list demo program. Traverses all elements of an integer list.
+r0: temp storage of value in list
+r2: address of start of list
+r3: address of end of list
+-------------------------------------------------------
+*/
+.org 0x1000  // Start at memory location 1000
+.text        // Code section
+.global _start
+_start:
+mov r4, #0
+mov r5, #0
+ldr    r2, =Data    // Store address of start of list
+ldr    r3, =_Data   // Store address of end of list
+sub r5,r3,r2
+mov r5, r5, lsr #2
+
+Loop:
+ldr    r0, [r2], #4 // Read address with post-increment (r0 = *r2, r2 += 4)
+add    r4, r4, #1
+cmp    r3, r2       // Compare current address with end of list
+bne    Loop         // If not at end, continue
+
+_stop:
+b _stop
+
+.data
+.align
+Data:
+.word   4,5,-9,0,3,0,8,-7,12    // The list of data
+_Data: // End of list address
+
+.end
